b7c0d601d41cde90c5ff1ea09ac4443868d7a06b
Jayme <Jayme@10.10.11.199>
2014-03-17 21:35:18 -0300

    commit inicial

---
+++ Questao3/src/Answer.java
@@ -0,0 +1,21 @@
+
+public class Answer {
+
+	/*orientação a objetos pressupõe uma organização de software em termos de
+	 * coleção de objetos discretos incorporando estrutura e comportamento próprios.
+	 * “Fornece um mapeamento direto entre o mundo  real e as unidades de organização
+	 * utilizadas no projeto”.
+	 * 
+	 * Vantagens:
+		Flexibilidade
+		Reusabilidade
+		Robustez
+		Modularidade
+	 * 
+	 * Elementos Básicos:
+	 * 	Objetos
+		Classes
+		Instâncias
+	 * 
+	 * */
+}
