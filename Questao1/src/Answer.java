6b108ef91802eb61f135eba00326a294357a47e6
Jayme <Jayme@10.10.11.199>
2014-03-17 21:10:12 -0300

    commit inicial

---
+++ Questao1/src/Answer.java
@@ -0,0 +1,12 @@
+
+public class Answer {
+
+	/* O compilador Java converte o código fonte em bytecodes que representam as tarefas
+	 * a serem executadas. Os bytecodes são executados pela JVM. A JVM armazena o programa
+	 * na memória para executá-lo. Há um processo de verificação dos bytecodes para 
+	 * certificar que eles são válidos.
+	 * A execução de programas Java, é feita através do interpretador java
+	 * e executa aplicações Java compiladas (bytecodes – extensão .class).
+	 */
+	
+}
