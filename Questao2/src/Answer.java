b17ab7c5255fa082767b9e250cc6e0bbac4469b8
Jayme <Jayme@10.10.11.199>
2014-03-17 21:29:42 -0300

    commit inicial

---
+++ Questao2/src/Answer.java
@@ -0,0 +1,28 @@
+
+public class Answer {
+
+	/*Um estado (atributos): Define os estados possíveis que um objeto pode assumir.
+	São os valores dos atributos (propriedades);
+	Ex. Lampada -> Acesa, Apagada.
+	
+	*Um Comportamento (métodos): São as funções que podem ser executadas por um 
+	determinado objeto. Corresponde aos métodos. ou seja, o que você pode fazer
+	com esse objeto.
+	Ex. Lampada -> Acender(); Apagar();
+	
+	Interface (assinaturas): Interface é um recurso da orientação a objeto utilizado em Java
+	que define ações que devem ser obrigatoriamente executadas,
+	mas que cada classe pode executar de forma diferente. Contém valores constantes
+	ou assinaturas de métodos que devem ser implementados dentro de uma classe.
+	Ex. interface nomeDaInterface{
+	}
+	métodoAbstrato (argumentos);
+		
+	Identidade: Um objeto é único, mesmo que o seu estado seja 
+	idêntico ao de outro.
+	Ex. Lampada -> Cód. De Fabricação 001; Incendescente;
+	
+	*/
+	
+	
+}
