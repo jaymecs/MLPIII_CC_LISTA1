3002d455dbca15c47e738c5824cb52aa9ebea572
Jayme <Jayme@JAYMEBADAMS>
2014-03-24 17:01:51 -0300

    commit inicial

---
+++ Questao4/src/Answer.java
@@ -0,0 +1,12 @@
+
+public class Answer {
+
+	/* Polimorfismo, que vem do grego "muitas formas", É o termo definido
+	 * para a possibilidade de se usar o mesmo elemento de forma diferente.
+	 * 
+	 * Ex.
+	 * 	Classe 1 possui 2 métodos: métodoA() e métodoB().
+		Classe 2 herda a classe 1.
+		Classe 2 reescreve todo o métodoA() que pertence a classe 1.
+	 * */
+}
